7adac9b2e6b13653904e6d21031736a4e9f61085
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-11-24 16:42:04 +0530

    refactor: commented code unfir for production

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
+# Github app id of the bot
 app_id = '634710'
 
+# private key to authenticate with github
+# location for production as dictated by the deployment web service used
 # bot certificate
 with open(
     os.path.normpath(os.path.expanduser('~/Code/certificates_keys/roody_ruler_github_bot.pem')), 
@@ -60,6 +63,6 @@ def bot():
     issue.create_comment(f"![Alt Text]({meme_url})")
     return 'ok'
 
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# For testing locally
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
