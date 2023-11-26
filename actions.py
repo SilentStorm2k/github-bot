76f83bbacc05e049b6320a015623fad2281d2aa4
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-11-26 18:29:26 +0530

    refactor: moved pr comment action to actions

--- actions.py
+++ actions.py
@@ -1,3 +1,5 @@
+from github import Github, GithubIntegration, Auth
+
 validEvents = ['issues', 'issue_comment', 'member', 'installation', 'check_suite', 'discussion', \
                 'discussion_comment', 'pull_request_review', 'pull_request_review_thread', \
                 'pull_request', 'pull_request_review_comment', 'status']
@@ -8,13 +10,44 @@ validStatus = ['assigned', 'closed', 'deleted', 'demilestoned', 'edited', 'label
                'enqueued', 'ready_for_review', 'review_request_removed', 'review_requested', \
                'synchronize', 'error', 'failure', 'pending', 'success']
 
-def execute(headers, payload):
+def execute(headers, payload, git_integration):
 
-    pass #place holder for now
+    # check if event is a Github PR creation event
+    if not all(k in payload.keys() for k in ['action', 'pull_request']) and \
+            payload['action'] == 'opened':
+        return "ok"
+
+    owner = payload['repository']['owner']['login']
+    repo_name = payload['repository']['name']
+
+    # Get a git connection as our bot
+    # Here is where we are getting the permission to talk to our bot
+    # and not as a python webservice
+    git_connection = Github(
+        login_or_token=git_integration.get_access_token(
+            git_integration.get_repo_installation(owner, repo_name).id
+        ).token
+    )
+
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+    issue = repo.get_issue(number=payload['number'])
+
+    # prone to change
+    # Call meme-api to get a random meme
+    response = requests.get(url='https://meme-api.com/gimme/wholesomememes')
+
+    if response.status_code != 200:
+        return 'ok'
+    
+    # Get the best resolution meme??
+    meme_url = response.json()['preview'][-1]
+    # create comment with random meme
+    issue.create_comment(f"![Alt Text]({meme_url})")
+    return 'ok'
 
 
-def isValidAction(eventType, eventStatus) {
-    if (not eventType in validEvents or not eventStatus in validStatus)
+def isValidAction(eventType, eventStatus):
+    if (not eventType in validEvents or not eventStatus in validStatus):
         return 'invalid event: make sure bot has access to ' + eventType
     return eventType
-}
--- app.py
+++ app.py
@@ -33,41 +33,7 @@ def bot():
     # get event payload
     payload = request.json
 
-    execute(headers, payload)
-
-    # check if event is a Github PR creation event
-    if not all(k in payload.keys() for k in ['action', 'pull_request']) and \
-            payload['action'] == 'opened':
-        return "ok"
-
-    owner = payload['repository']['owner']['login']
-    repo_name = payload['repository']['name']
-
-    # Get a git connection as our bot
-    # Here is where we are getting the permission to talk to our bot
-    # and not as a python webservice
-    git_connection = Github(
-        login_or_token=git_integration.get_access_token(
-            git_integration.get_repo_installation(owner, repo_name).id
-        ).token
-    )
-
-    repo = git_connection.get_repo(f"{owner}/{repo_name}")
-
-    issue = repo.get_issue(number=payload['number'])
-
-    # prone to change
-    # Call meme-api to get a random meme
-    response = requests.get(url='https://meme-api.com/gimme/wholesomememes')
-
-    if response.status_code != 200:
-        return 'ok'
-    
-    # Get the best resolution meme??
-    meme_url = response.json()['preview'][-1]
-    # create comment with random meme
-    issue.create_comment(f"![Alt Text]({meme_url})")
-    return 'ok'
+    return execute(headers, payload, git_integration)
 
 # For testing locally
 if __name__ == '__main__':
